96dff4130193b251af27013ef1851a59406ade7a
amanfrontdev <amanfrontdev@gmail.com>
2024-04-26 12:42:17 +0530

    changes done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,36 +7,46 @@ const Navbar = () => {
   const [hide, setHide] = useState(false)
 
   return (
-    <div className="bg-DarkBlack relative mx-auto max-w-[1920px] border-black flex flex-row justify-between px-2 sm:px-10 items-center py-4">
+    <div className="bg-DarkBlack z-20 relative mx-auto max-w-[1920px] border-black flex flex-row justify-between px-2 sm:px-10 items-center py-4">
       <div>
         <div className="w-40 h-12">
           <img src="img/navbar/Logo.svg" alt="logo image" />
         </div>
       </div>
       <div className="flex flex-row justify-center items-center">
-        <ul className={` md:flex flex-col md:flex-row text-[16px] transition-all duration-500 ease-in-out bg-DarkGray  ${hide ? "flex left-[0px] py-10 md:py-0 " : "-left-[900px] md:left-0 hidden"} w-full top-20 md:top-0 md:h-auto absolute md:relative  gap-4 justify-center items-center`}>
+        <ul className={` md:flex flex-col md:flex-row text-[16px] z-10 transition-all duration-500 ease-in-out bg-DarkGray  ${hide ? "flex top-[76px] left-0 py-10 md:py-0 " : "-top-[300px] left-0"} w-full top-20 md:top-0 md:h-auto absolute md:relative  gap-4 justify-center items-center`}>
           <li className="">
-            <Link to="/" className="text-customLightPurple cursor-pointer px-2 py-[6px] rounded-[6px] hover:bg-white transition-all duration-300 ease-in-out font-medium" >
+            <Link to="/" className="text-customLightPurple cursor-pointer px-2 py-[6px] rounded-[6px] hover:bg-white transition-all duration-300 ease-in-out font-medium"
+              onClick={() => setHide(false)}
+            >
               Home
             </Link>
           </li>
           <li>
-            <Link to="/about" className="text-customLightPurple cursor-pointer px-2 py-[6px] rounded-[6px] hover:bg-white transition-all duration-300 ease-in-out font-medium" >
+            <Link to="/about" className="text-customLightPurple cursor-pointer px-2 py-[6px] rounded-[6px] hover:bg-white transition-all duration-300 ease-in-out font-medium"
+              onClick={() => setHide(false)}
+            >
               About
             </Link>
           </li>
           <li>
-            <Link to="/it-services" className="text-customLightPurple cursor-pointer px-2 py-[6px] rounded-[6px] hover:bg-white transition-all duration-300 ease-in-out font-medium" >
+            <Link to="/it-services" className="text-customLightPurple cursor-pointer px-2 py-[6px] rounded-[6px] hover:bg-white transition-all duration-300 ease-in-out font-medium"
+              onClick={() => setHide(false)}
+            >
               Services
             </Link>
           </li>
           <li>
-            <Link to="/it-education" className="text-customLightPurple cursor-pointer px-2 py-[6px] rounded-[6px] hover:bg-white transition-all duration-300 ease-in-out font-medium" >
+            <Link to="/it-education" className="text-customLightPurple cursor-pointer px-2 py-[6px] rounded-[6px] hover:bg-white transition-all duration-300 ease-in-out font-medium"
+              onClick={() => setHide(false)}
+            >
               Education
             </Link>
           </li>
           <li>
-            <Link to="/contact" className="text-customLightPurple cursor-pointer px-2 py-[6px] rounded-lg hover:bg-white transition-all duration-300 ease-in-out font-medium" >
+            <Link to="/contact" className="text-customLightPurple cursor-pointer px-2 py-[6px] rounded-lg hover:bg-white transition-all duration-300 ease-in-out font-medium"
+              onClick={() => setHide(false)}
+            >
               Contact
             </Link>
           </li>
