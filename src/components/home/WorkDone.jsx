67fbe71911ebaef0cf36c3d5c8f693db8d791d84
amanfrontdev <amanfrontdev@gmail.com>
2024-04-30 12:58:34 +0530

    changed links in footer and navbar

--- src/components/home/WorkDone.jsx
+++ src/components/home/WorkDone.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-const WorkDone = ({ heading, para, num }) => {
+const WorkDone = ({ heading, para, num, arrow }) => {
     return (
-        <>
-            <div className="w-[250px] sm:w-[300px] h-[200px] sm:h-[260px] shadowBox flex-col bg-transparent hover:bg-customPurple  text-white p-4 rounded-lg flex">
+        <div className='relative'>
+            <div className="w-[250px] sm:w-[300px] h-[200px] sm:h-[260px] shadowBox flex-col bg-black hover:bg-customPurple  text-white p-4 rounded-lg flex">
                 <p className='text-2xl sm:text-6xl w-full flex justify-end'>
                     {num}
                 </p>
@@ -14,12 +14,14 @@ const WorkDone = ({ heading, para, num }) => {
                     {para}
                 </p>
             </div>
-            <div className="flex items-center justify-center animate-ping">
-                <svg className="w-6 h-6 text-white ml-4 rotate-90 sm:rotate-0" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 5l7 7-7 7" />
-                </svg>
-            </div>
-        </>
+            {arrow === "true" &&
+                <div className="flex absolute -bottom-10 left-[40%] md:-right-12 md:top-[50%] items-center justify-center animate-ping">
+                    <svg className="w-6 h-6 text-white ml-4 rotate-90 md:rotate-0" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 5l7 7-7 7" />
+                    </svg>
+                </div>
+            }
+        </div>
     )
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,4 @@
 import React from 'react'
-import TypeWriter from '../components/TypeWriter/TypeWriter'
-import Paragraph from '../components/services/Paragraph'
 import Cards from '../components/services/Cards'
 import { cardsData, cardsData1, cardsData2, cardsData3 } from '../data/Data'
 import MiddleText from '../components/home/MiddleText'
@@ -10,17 +8,14 @@ import WorkDone from '../components/home/WorkDone'
 
 const Home = () => {
     return (
-        <section className='max-w-[1920px] mx-auto'>
+        <section className='max-w-[1920px] mx-auto -z-10'>
             {/* first section  */}
-            <div className="flex flex-col md:flex-row h-[90vh] bg-slate-300">
+            <div className="flex flex-col md:flex-row h-[450px] sm:h-[600px] bg-DarkGray">
                 <div className="w-full flex pt-32 ">
                     <div className=" mx-auto text-center px-1">
-                        <div className='bg-slate-500'>
-                            <TypeWriter />
-                        </div>
                         <h1 className="text-3xl sm:text-5xl md:text-7xl font-bold text-customLightPurple mb-4">Creating strategies for </h1>
-                        <p className="text-xl sm:text-2xl md:text-4xl text-DarkGray">Creating strategies for cutting-edge technology trends.</p>
-                        <p className='mx-auto text-xs max-w-[600px] lg:max-w-[800px] sm:text-md md:text-xl mt-2 text-DarkGray/90 font-medium'>OFFERING EFFECTIVE SOLUTIONS, OUR SOFTWARE DEVELOPMENT COMPANY SPECIALIZES IN CRAFTING INNOVATIVE SOFTWARE PRODUCTS</p>
+                        <p className="text-xl sm:text-2xl md:text-4xl text-white">Creating strategies for cutting-edge technology trends.</p>
+                        <p className='mx-auto text-xs max-w-[600px] lg:max-w-[800px] sm:text-md md:text-xl mt-2 text-white/90 font-medium'>OFFERING EFFECTIVE SOLUTIONS, OUR SOFTWARE DEVELOPMENT COMPANY SPECIALIZES IN CRAFTING INNOVATIVE SOFTWARE PRODUCTS</p>
                     </div>
                 </div>
             </div>
@@ -43,18 +38,20 @@ const Home = () => {
             </div>
 
             {/* fourth section  */}
-            <div>
+            <div className='bg-DarkGray py-20'>
                 <MiddleText heading="How work is done in" para="Loopdrill" />
-                <div className="flex flex-row max-w-[1920px] px-10 mt-20 pb-20 justify-center gap-6 flex-wrap ">
-                    <WorkDone num="01" heading="Information Gathering" para="We meticulously collect every detail, beginning with your vision and delving deep into the core ideas within your mind." />
-                    <WorkDone num="02" heading="Research and brainstorm" para="Thoroughly analyzing your competitors' businesses, we craft unique selling points that set your business apart in the market." />
-                    <WorkDone num="03" heading="Design blueprintg" para="A collaborative effort combining diverse ideas and thorough research to pave a path where everything can flow smoothly" />
+                <div className="flex flex-row max-w-[1920px] px-10 mt-20 pb-20 justify-center gap-16 md:gap-24 flex-wrap ">
+                    <WorkDone arrow="true" num="01" heading="Information Gathering" para="We meticulously collect every detail, beginning with your vision and delving deep into the core ideas within your mind." />
+                    <WorkDone arrow="true" num="02" heading="Research and brainstorm" para="Thoroughly analyzing your competitors' businesses, we craft unique selling points that set your business apart in the market." />
+                    <WorkDone arrow="true" num="03" heading="Design blueprintg" para="A collaborative effort combining diverse ideas and thorough research to pave a path where everything can flow smoothly" />
 
-                    <WorkDone num="04" heading="Development" para="All the gathered resources will be utilized, transforming into reality through the enchantment of coding." />
-                    <WorkDone num="05" heading="Code review" para="Dedicated to addressing both minor hiccups and major challenges in the project, resolving them promptly." />
-                    <WorkDone num="06" heading="Testing" para="Once all preparations are complete on the developer's end, the focus shifts to identifying and resolving any bugs that may arise." />
+                    <WorkDone arrow="true" num="04" heading="Development" para="All the gathered resources will be utilized, transforming into reality through the enchantment of coding." />
+                    <WorkDone arrow="true" num="05" heading="Code review" para="Dedicated to addressing both minor hiccups and major challenges in the project, resolving them promptly." />
+                    <WorkDone arrow="true" num="06" heading="Testing" para="Once all preparations are complete on the developer's end, the focus shifts to identifying and resolving any bugs that may arise." />
 
-                    <WorkDone num="07" heading="Project deliver" para="With everything in place, your project is poised to launch into the market, ready to achieve remarkable success." />
+                    <div className=''>
+                        <WorkDone arrow="false" num="07" heading="Project deliver" para="With everything in place, your project is poised to launch into the market, ready to achieve remarkable success." />
+                    </div>
                 </div>
 
 
